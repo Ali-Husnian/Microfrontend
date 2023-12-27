8cdb56af978a5205c837b5a151554f596f58a624
unknown <pkihusnain220@gmail.com>
2023-12-27 15:17:47 +0500

    Image Laders

---
+++ 05_image-loaders/src/index.js
@@ -0,0 +1,23 @@
+import _ from "lodash";
+import style from "./index.css";
+import "./clearButton";
+import logo from "./assets/webpack-logo.png";
+
+const btn1 = document.getElementById("button1");
+const logoEl = document.getElementById("logo-img");
+
+btn1.addEventListener("click", function () {
+  const clicChange = document.getElementById("header");
+  clicChange.innerHTML = "It is also called microfrontend!";
+
+  const listItem = ["Acadmind", "The Netninja", "Stifan Grander"];
+  const ul = document.getElementById("shoppingList");
+  _.forEach(listItem, function (item) {
+    const tempItem = document.createElement("li");
+    tempItem.innerHTML = item;
+    ul.appendChild(tempItem);
+  });
+});
+
+btn1.classList.add([style.button]);
+logoEl.src = logo;
